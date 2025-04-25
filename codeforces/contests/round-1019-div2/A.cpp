5d759a2c9e2621f083d635c8607d4ee588909ae8
asatpathy314 <asatpathy314@gmail.com>
2025-04-24 20:36:50 -0400

    solution for round-1019-div2 A

--- codeforces/contests/round-1019-div2/A.cpp
+++ codeforces/contests/round-1019-div2/A.cpp
@@ -9,6 +9,25 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef vector<int> vi;
 
+
+int solve() {
+  int l, num;
+  cin >> l;
+  set<int> nums;
+
+  for (int i = 0; i < l; ++i) {
+    cin >> num;
+    nums.insert(num);
+  }
+
+  return nums.size();
+}
+
 int main() {
-  
+    int c, max;
+    cin >> c;
+    for (int i = 0; i < c; ++i) {
+      max = solve();
+      cout << max << endl;
+    }
 }
--- codeforces/contests/round-1019-div2/B.cpp
+++ codeforces/contests/round-1019-div2/B.cpp
@@ -1,14 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define rep(i, a, b) for(int i = a; i < (b); ++i)
-#define trav(a, x) for(auto& a : x)
-#define all(x) x.begin(), x.end()
-#define sz(x) (int)(x).size()
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef vector<int> vi;
+int solve(){
+    int l;
+    string s;
+    cin >> l >> s;
 
-int main() {
-  
+    // 1) Find best_gain exactly as before
+    const int INF = l + 5;
+    vector<int> first_cut(4, INF), last_cut(4, -INF);
+    for(int c = 0; c < l; ++c){
+        int L = (c == 0 ? 0 : s[c-1] - '0');
+        int R =      s[c]   - '0';
+        int id = L*2 + R;
+        first_cut[id] = min(first_cut[id], c);
+        last_cut [id] = max(last_cut [id], c);
+    }
+    int gain[4][4];
+    for(int id1 = 0; id1 < 4; ++id1){
+        int a = id1/2, b = id1%2;
+        for(int id2 = 0; id2 < 4; ++id2){
+            int c_ = id2/2, d = id2%2;
+            int oldL = (a!=b), oldR = (c_!=d);
+            int newL = (a!=c_), newR = (b!=d);
+            gain[id1][id2] = oldL + oldR - newL - newR;
+        }
+    }
+    int best_gain = 0;
+    for(int id1=0; id1<4; ++id1){
+      if(first_cut[id1] == INF) continue;
+      for(int id2=0; id2<4; ++id2){
+        if(last_cut[id2] < first_cut[id1]) continue;
+        best_gain = max(best_gain, gain[id1][id2]);
+      }
+    }
+
+    // 2) Compute original transitions with implicit leading 0
+    int transitions = 0;
+    int prev = 0;
+    for(char c : s){
+        int cur = c - '0';
+        if(cur != prev) ++transitions;
+        prev = cur;
+    }
+
+    // 3) Final cost = transitions + length − best_gain
+    return transitions + l - best_gain;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int c;
+    cin >> c;                // ← must read number of test cases!
+    while(c--){
+        cout << solve() << "\n";
+    }
+    return 0;
 }
